e29827ab16ad9b155d21c51c61a88b9706f99fe4
alyonatovt <126613455+alyonatovt@users.noreply.github.com>
2023-10-26 12:05:04 +0300

    Update Student.java

--- src/com/tovt/Student.java
+++ src/com/tovt/Student.java
@@ -1,9 +1,9 @@
 package com.tovt;
 
 public class Student {
-    String name;
-    String surname;
-    int age;
+   private String name;
+   private String surname;
+   private int age;
     String group;
 
     public Student(String name, String surname, int age, String group) {
@@ -15,9 +15,33 @@ public class Student {
 void infoStudent(){
     System.out.println("Name:"+ name + " "+ "Surname:"+ surname + " "+ "group:"+ group);
 }
-public int getAge() {
-    return age;
-}
+    void infoStudent(String name,String surname,int age){
+        System.out.println("Name:"+ name + " "+ "Surname:"+ surname + " "+ "age:"+ age);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 
 }
 
